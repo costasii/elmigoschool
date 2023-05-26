92315d5b4477d4f8608e5bd6aad7c264330b559d
Mirta <medak.mirta@gmail.com>
2023-05-26 18:17:24 +0200

    newprices

--- src/scripts/discounts.js
+++ src/scripts/discounts.js
@@ -2,20 +2,29 @@ function showPricelist() {
     var ele = document.getElementsByName('offer');
     var general = document.getElementById("table-general");
     var u16 = document.getElementById("table-u16");
-    var students = document.getElementById("table-students");
-
+    var G2 = document.getElementById("table-G2");
+    var G3 = document.getElementById("table-G3");
+    
     if (ele[0].checked) {
+        G2.style.display = "block";
+        G3.style.display = "none";
+        u16.style.display = "none";
+        general.style.display = "none";
        
+    } else if (ele[1].checked) {
+        G2.style.display = "none";
+        G3.style.display = "block";
+        u16.style.display = "none";
+        general.style.display = "none";
+    } else if (ele[2].checked) {
+        G2.style.display = "none";
+        G3.style.display = "none";
         u16.style.display = "block";
         general.style.display = "none";
-        students.style.display = "none";
-    } else if (ele[1].checked) {
-        u16.style.display = "none";
-        general.style.display = "none";
-        students.style.display = "block";
     } else {
+        G2.style.display = "none";
+        G3.style.display = "none";
         u16.style.display = "none";
         general.style.display = "block";
-        students.style.display = "none";
     }
 }
